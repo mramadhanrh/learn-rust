c92ad041df6b686eb8ec105c00e646f173280df7
Ramadhan Rahmat <hi.mramadhanrh@gmail.com>
2026-01-19 05:00:33 +0700

    feature: add module 01 with examples for variables, flow control, and macros

---
+++ module-01-basics/src/README.md
@@ -0,0 +1,5 @@
+cargo run --bin 01-basics-variable-and-immutability
+cargo run --bin 02-declarative-macros
+cargo run --bin 03-variables-and-types
+cargo run --bin 04-flow-control
+cargo run --bin 05-enums
---
+++ module-01-basics/src/main.rs
@@ -0,0 +1,12 @@
+// This is the default main.rs
+// Run specific examples using:
+// cargo run --bin 01-basics-variable-and-immutability
+// cargo run --bin 02-declarative-macros
+// cargo run --bin 03-variables-and-types
+// cargo run --bin 04-flow-control
+// cargo run --bin 05-enums
+
+fn main() {
+    println!("Module 01 - Rust Basics");
+    println!("Run specific examples with: cargo run --bin <example-name>");
+}
