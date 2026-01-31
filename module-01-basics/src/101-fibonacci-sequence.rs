3e8a8fdf0004cc0308a3a7f357296c252841e8ec
Ramadhan Rahmat <hi.mramadhanrh@gmail.com>
2026-01-31 22:04:29 +0700

    feature: refactor main function and add get_reference_value for demonstration of ownership and borrowing

--- module-01-basics/src/101-fibonacci-sequence.rs
+++ module-01-basics/src/101-fibonacci-sequence.rs
@@ -1,12 +1,26 @@
-fn main() {
+fn get_reference_value() {
+    let a = 42;
+    let b = &a;
+
+    println!("{b}");
+
+    let mut c = *b;
+    c += 1;
+
+    println!("{c}");
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+
+    // println!("{s1}"); // This will error since s1 already moved to s2, All expensive types that uses heap, like string, vec, array will be moved when reassigned to other params, while number will be copied
+    println!("{s2}") // This is works
+}
+
+fn get_fibonacci_sequence() {
     let mut fibonacci_sequence: Vec<i128> = vec![];
     let length: i128 = 100;
 
     for x in 0..length {
-        if fibonacci_sequence.last() == Some(&1) {
-            println!("why");
-        }
-
         if fibonacci_sequence.len() <= 1 {
             fibonacci_sequence.push(x);
             let last_item = fibonacci_sequence.last();
@@ -23,4 +37,9 @@ fn main() {
         let last_item = fibonacci_sequence.last();
         println!("Item Number {x}: {:?}", last_item);
     }
+}
+
+fn main() {
+    get_fibonacci_sequence();
+    get_reference_value();
 }
